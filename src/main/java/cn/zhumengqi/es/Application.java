96dfdb0ba8c9ec25eee467baa57d39b0d726d76f
zhumengqi <mickey.zh.cn@gmail.com>
2017-05-01 13:50:13 +0800

    添加Application

---
+++ src/main/java/cn/zhumengqi/es/Application.java
@@ -0,0 +1,14 @@
+package cn.zhumengqi.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhumengqi on 17-5-1.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
